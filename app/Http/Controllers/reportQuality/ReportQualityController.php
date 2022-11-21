da68c806aae5b50dbb56c4abd81c0a9769fb0dc7
fernando2523 <74076122+fernando2523@users.noreply.github.com>
2022-11-22 05:39:56 +0700

    FERNANDO - TERBARU - 05:39

--- app/Http/Controllers/reportQuality/ReportQualityController.php
+++ app/Http/Controllers/reportQuality/ReportQualityController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
 use Illuminate\Support\Str;
 use App\Models\Product;
+use App\Models\Store;
 use App\Models\Sale;
 
 class ReportQualityController extends Controller
@@ -17,25 +18,64 @@ class ReportQualityController extends Controller
     public function quality()
     {
         $title = "Report Quality";
-
-        $get_qty = Sale::sum('qty');
-        $get_gross = Sale::all();
-        $get_discitem = Sale::all()->sum('diskon_item');
-        $get_costs = Sale::all();
+        $store = Store::all();
 
         return view('reportQuality/quality', compact(
             'title',
-            'get_qty',
-            'get_gross',
-            'get_discitem',
-            'get_costs',
+            'store',
         ));
     }
 
-    public function tablereportquality(Request $request)
+    public function load_report_quality(Request $request)
     {
         if ($request->ajax()) {
-            $product = Sale::with('quality_qtys', 'quality_gross', 'quality_disc_item', 'quality_costs')->groupBy('quality')->get();
+            $store = $request->store;
+            $start = $request->start;
+            $end = $request->end;
+
+            if ($store === 'ALL') {
+                $get_qty = Sale::all()->whereBetween('tanggal', [$start, $end])->sum('qty');
+                $get_gross = Sale::all()->whereBetween('tanggal', [$start, $end]);
+                $get_discitem = Sale::all()->whereBetween('tanggal', [$start, $end])->sum('diskon_item');
+                $get_costs = Sale::all()->whereBetween('tanggal', [$start, $end]);
+            } else {
+                $get_qty = Sale::all()->where('id_store', $store)->whereBetween('tanggal', [$start, $end])->sum('qty');
+                $get_gross = Sale::all()->where('id_store', $store)->whereBetween('tanggal', [$start, $end]);
+                $get_discitem = Sale::all()->where('id_store', $store)->whereBetween('tanggal', [$start, $end])->sum('diskon_item');
+                $get_costs = Sale::all()->where('id_store', $store)->whereBetween('tanggal', [$start, $end]);
+            }
+
+            return view('reportQuality/load_report_quality', compact(
+                'store',
+                'start',
+                'end',
+                'get_gross',
+                'get_qty',
+                'get_discitem',
+                'get_costs',
+            ));
+        }
+    }
+
+    public function tablereportquality(Request $request, $store, $start, $end)
+    {
+        if ($request->ajax()) {
+
+            if ($store === 'ALL') {
+                $product = Sale::latest()
+                    ->selectRaw('*,SUM(qty) as qty,SUM(selling_price*qty) as selling_price,SUM(diskon_item) as diskon_item,SUM(m_price*qty) as costs')
+                    ->whereBetween('tanggal', [$start, $end])
+                    ->groupBy('quality')
+                    ->get();
+            } else {
+                $product = Sale::latest()
+                    ->selectRaw('*,SUM(qty) as qty,SUM(selling_price*qty) as selling_price,SUM(diskon_item) as diskon_item,SUM(m_price*qty) as costs')
+                    ->where('id_store', $store)
+                    ->whereBetween('tanggal', [$start, $end])
+                    ->groupBy('quality')
+                    ->get();
+            }
+
             return DataTables::of($product)
                 ->addIndexColumn()
                 ->addColumn('action', function () {
