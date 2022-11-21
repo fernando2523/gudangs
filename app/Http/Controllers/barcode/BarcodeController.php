b680439dfe6a3888e40c23dda0ed7521313672e1
fernando2523 <74076122+fernando2523@users.noreply.github.com>
2022-11-22 01:33:04 +0700

    fernando - 01:32

--- app/Http/Controllers/barcode/BarcodeController.php
+++ app/Http/Controllers/barcode/BarcodeController.php
@@ -99,14 +99,15 @@ class BarcodeController extends Controller
             [
                 'format' => 'A5',
                 'orientation' => 'L',
-                'margin_left' => 1,
-                'margin_right ' => 1,
-                'margin_top' => 0,
-                'margin_bottom' => 0,
+                'margin_left' => -0,
+                'margin_right ' => -20,
+                'margin_top' => 5,
+                'margin_bottom' => -10,
                 'margin_header' => 0,
                 'margin_footer' => 0,
             ]
         );
+
         return $pdf->stream('document.pdf');
     }
 }
--- resources/views/print/printtest.blade.php
+++ resources/views/print/printtest.blade.php
@@ -1,30 +1,295 @@
-<table class="table">
-    <tbody align="center">
-        <tr style="background-color: aqua;">
-            <td style="width: 33%">
-                <span class="text-center">OLD SKOOL CLASSICS BLACK WHITE HYPE...</span>
-            </td>
-            <td style="width: 33%">
-                <span class="text-center">OLD SKOOL CLASSICS BLACK WHITE HYPE...</span>
-            </td>
-            <td style="width: 33%">
-                <span class="text-center">OLD SKOOL CLASSICS BLACK WHITE HYPE...</span>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <barcode code="1122200012.41" type="C39" height="2" size="0.7" />
-            </td>
-        </tr>
-        <tr>
-            <td>
-                #221021132
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <span class="text-center">1122200012.41</span>
-            </td>
-        </tr>
+<table class="table" style="margin-right: -20px;">
+    <tbody>
+        @for ($i = 0; $i < 5; $i++)
+            <tr>
+                <td align="center" style="padding-left: 23px;padding-bottom: 36px;">
+                    <div class="mpdf_toc">
+                        <span style="font-size: 12px;">OLD
+                            SKOOL CLASSICS
+                            BLACK WHITE
+                            ...</span>
+                        <span align="center">
+                            <barcode code="1122200012.41" type="C39" height="2.7" size="0.7" />
+                        </span>
+                        <span class="text-center fw-bold" style="font-size: 14px;font-weight: bold;">#221021132
+                            | </span>
+                        <span>1122200012.41</span>
+                    </div>
+                </td>
+                <td align="center" style="padding-left: 23px;padding-bottom: 36px;">
+                    <div class="mpdf_toc">
+                        <span style="font-size: 12px;">OLD
+                            SKOOL CLASSICS
+                            BLACK WHITE
+                            ...</span>
+                        <span align="center">
+                            <barcode code="1122200012.41" type="C39" height="2.7" size="0.7" />
+                        </span>
+                        <span class="text-center fw-bold" style="font-size: 14px;font-weight: bold;">#221021132
+                            | </span>
+                        <span>1122200012.41</span>
+                    </div>
+                </td>
+                <td align="center" style="padding-left: 23px;padding-bottom: 36px;">
+                    <div class="mpdf_toc">
+                        <span style="font-size: 12px;">OLD
+                            SKOOL CLASSICS
+                            BLACK WHITE
+                            ...</span>
+                        <span align="center">
+                            <barcode code="1122200012.41" type="C39" height="2.7" size="0.7" />
+                        </span>
+                        <span class="text-center fw-bold" style="font-size: 14px;font-weight: bold;">#221021132
+                            | </span>
+                        <span>1122200012.41</span>
+                    </div>
+                </td>
+            </tr>
+
+            <tr>
+                <td align="center" style="padding-left: 23px;padding-bottom: 36px;">
+                    <div class="mpdf_toc">
+                        <span style="font-size: 12px;">OLD
+                            SKOOL CLASSICS
+                            BLACK WHITE
+                            ...</span>
+                        <span align="center">
+                            <barcode code="1122200012.41" type="C39" height="2.7" size="0.7" />
+                        </span>
+                        <span class="text-center fw-bold" style="font-size: 14px;font-weight: bold;">#221021132
+                            | </span>
+                        <span>1122200012.41</span>
+                    </div>
+                </td>
+                <td align="center" style="padding-left: 23px;padding-bottom: 36px;">
+                    <div class="mpdf_toc">
+                        <span style="font-size: 12px;">OLD
+                            SKOOL CLASSICS
+                            BLACK WHITE
+                            ...</span>
+                        <span align="center">
+                            <barcode code="1122200012.41" type="C39" height="2.7" size="0.7" />
+                        </span>
+                        <span class="text-center fw-bold" style="font-size: 14px;font-weight: bold;">#221021132
+                            | </span>
+                        <span>1122200012.41</span>
+                    </div>
+                </td>
+                <td align="center" style="padding-left: 23px;padding-bottom: 36px;">
+                    <div class="mpdf_toc">
+                        <span style="font-size: 12px;">OLD
+                            SKOOL CLASSICS
+                            BLACK WHITE
+                            ...</span>
+                        <span align="center">
+                            <barcode code="1122200012.41" type="C39" height="2.7" size="0.7" />
+                        </span>
+
+
+                        <span class="text-center fw-bold" style="font-size: 14px;font-weight: bold;">#221021132
+                            | </span>
+                        <span>1122200012.41</span>
+                    </div>
+                </td>
+            </tr>
+
+            <tr>
+                <td align="center" style="padding-left: 23px;padding-bottom: 36px;">
+                    <div class="mpdf_toc">
+                        <span style="font-size: 12px;">OLD
+                            SKOOL CLASSICS
+                            BLACK WHITE
+                            ...</span>
+                        <span align="center">
+                            <barcode code="1122200012.41" type="C39" height="2.7" size="0.7" />
+                        </span>
+
+
+                        <span class="text-center fw-bold" style="font-size: 14px;font-weight: bold;">#221021132
+                            | </span>
+                        <span>1122200012.41</span>
+                    </div>
+                </td>
+                <td align="center" style="padding-left: 23px;padding-bottom: 36px;">
+                    <div class="mpdf_toc">
+                        <span style="font-size: 12px;">OLD
+                            SKOOL CLASSICS
+                            BLACK WHITE
+                            ...</span>
+                        <span align="center">
+                            <barcode code="1122200012.41" type="C39" height="2.7" size="0.7" />
+                        </span>
+
+
+                        <span class="text-center fw-bold" style="font-size: 14px;font-weight: bold;">#221021132
+                            | </span>
+                        <span>1122200012.41</span>
+                    </div>
+                </td>
+                <td align="center" style="padding-left: 23px;padding-bottom: 36px;">
+                    <div class="mpdf_toc">
+                        <span style="font-size: 12px;">OLD
+                            SKOOL CLASSICS
+                            BLACK WHITE
+                            ...</span>
+                        <span align="center">
+                            <barcode code="1122200012.41" type="C39" height="2.7" size="0.7" />
+                        </span>
+
+
+                        <span class="text-center fw-bold" style="font-size: 14px;font-weight: bold;">#221021132
+                            | </span>
+                        <span>1122200012.41</span>
+                    </div>
+                </td>
+            </tr>
+
+            <tr>
+                <td align="center" style="padding-left: 23px;padding-bottom: 36px;">
+                    <div class="mpdf_toc">
+                        <span style="font-size: 12px;">OLD
+                            SKOOL CLASSICS
+                            BLACK WHITE
+                            ...</span>
+                        <span align="center">
+                            <barcode code="1122200012.41" type="C39" height="2.7" size="0.7" />
+                        </span>
+
+
+                        <span class="text-center fw-bold" style="font-size: 14px;font-weight: bold;">#221021132
+                            | </span>
+                        <span>1122200012.41</span>
+                    </div>
+                </td>
+                <td align="center" style="padding-left: 23px;padding-bottom: 36px;">
+                    <div class="mpdf_toc">
+                        <span style="font-size: 12px;">OLD
+                            SKOOL CLASSICS
+                            BLACK WHITE
+                            ...</span>
+                        <span align="center">
+                            <barcode code="1122200012.41" type="C39" height="2.7" size="0.7" />
+                        </span>
+
+
+                        <span class="text-center fw-bold" style="font-size: 14px;font-weight: bold;">#221021132
+                            | </span>
+                        <span>1122200012.41</span>
+                    </div>
+                </td>
+                <td align="center" style="padding-left: 23px;padding-bottom: 36px;">
+                    <div class="mpdf_toc">
+                        <span style="font-size: 12px;">OLD
+                            SKOOL CLASSICS
+                            BLACK WHITE
+                            ...</span>
+                        <span align="center">
+                            <barcode code="1122200012.41" type="C39" height="2.7" size="0.7" />
+                        </span>
+
+
+                        <span class="text-center fw-bold" style="font-size: 14px;font-weight: bold;">#221021132
+                            | </span>
+                        <span>1122200012.41</span>
+                    </div>
+                </td>
+            </tr>
+        @endfor
     </tbody>
 </table>
+<style>
+    div.rounded {
+        border: 1mm dashed #220044;
+        background-color: #f0f2ff;
+        border-radius: 3mm / 3mm;
+        background-clip: border-box;
+        padding: 1em;
+    }
+
+    div.mpdf_toc {
+        font-family: sans-serif;
+        font-size: 11pt;
+    }
+
+    a.mpdf_toc_a {
+        text-decoration: none;
+        color: black;
+    }
+
+    /* Whole line level 0 */
+    div.mpdf_toc_level_0 {
+        line-height: 1.5;
+        margin-left: 0;
+        padding-right: 2em;
+    }
+
+    /* padding-right should match e.g <dottab outdent="2em" /> 0 is default */
+    /* Title level 0 - may be inside <a> */
+    span.mpdf_toc_t_level_0 {
+        font-weight: bold;
+    }
+
+    /* Page no. level 0 - may be inside <a> */
+    span.mpdf_toc_p_level_0 {}
+
+    /* Whole line level 1 */
+    div.mpdf_toc_level_1 {
+        margin-left: 2em;
+        text-indent: -2em;
+        padding-right: 2em;
+    }
+
+    /* padding-right should match <dottab outdent="2em" /> 2em is default */
+    /* Title level 1 */
+    span.mpdf_toc_t_level_1 {
+        font-style: italic;
+        font-weight: bold;
+    }
+
+    /* Page no. level 1 - may be inside <a> */
+    span.mpdf_toc_p_level_1 {}
+
+    /* Whole line level 2 */
+    div.mpdf_toc_level_2 {
+        margin-left: 4em;
+        text-indent: -2em;
+        padding-right: 2em;
+    }
+
+    /* padding-right should match <dottab outdent="2em" /> 2em is default */
+    /* Title level 2 */
+    span.mpdf_toc_t_level_2 {}
+
+    /* Page no. level 2 - may be inside <a> */
+    span.mpdf_toc_p_level_2 {}
+</style>
+{{-- 
+<div class="mpdf_toc" id="mpdf_toc_0">
+    <div class="mpdf_toc_level_0">
+        <a class="mpdf_toc_a" href="#__mpdfinternallink_1">
+            <span class="mpdf_toc_t_level_0">Section 1</span>
+        </a>
+        <dottab outdent="2em" />
+        <a class="mpdf_toc_a" href="#__mpdfinternallink_1">
+            <span class="mpdf_toc_p_level_0">5</span>
+        </a>
+    </div>
+    <div class="mpdf_toc_level_1">
+        <a class="mpdf_toc_a" href="#__mpdfinternallink_2">
+            <span class="mpdf_toc_t_level_1">Chapter 1</span>
+        </a>
+        <dottab outdent="2em" />
+        <a class="mpdf_toc_a" href="#__mpdfinternallink_2">
+            <span class="mpdf_toc_p_level_1">5</span>
+        </a>
+    </div>
+    <div class="mpdf_toc_level_2">
+        <a class="mpdf_toc_a" href="#__mpdfinternallink_3">
+            <span class="mpdf_toc_t_level_2">Topic 1</span>
+        </a>
+        <dottab outdent="2em" />
+        <a class="mpdf_toc_a" href="#__mpdfinternallink_3">
+            <span class="mpdf_toc_p_level_2">5</span>
+        </a>
+    </div>
+</div> --}}
